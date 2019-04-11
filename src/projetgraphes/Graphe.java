28d6ad31b32e58d02be1bed80d8d35db06edd5cf
Dorian <dorian.naaji@gmail.com>
2019-04-11 09:40:26 +0200

    graphe et affichage

--- src/projetgraphes/Graphe.java
+++ src/projetgraphes/Graphe.java
@@ -24,28 +24,48 @@ public class Graphe
     /**
      * see : 
      * https://www.caveofprogramming.com/java/java-file-reading-and-writing-files-in-java.html
-     * QUESTION 1.
+     * QUESTION 1 : permet de charger tous les mots d'un fichier et de les ajouter à la liste de sommet de notre graphe de mots de taille i
      */
     public void load(String filename)
     {
+        // le mot qui va être lu
         String mot = null;
-
         try
         {
             FileReader reader = new FileReader(filename);
             BufferedReader buffer = new BufferedReader(reader);
+            // tant qu'il y a des mots dans le fichier
             while( (mot = buffer.readLine()) != null)
             {
+                // on crée un sommet à partir du mot courant du fichier ( de la ligne courante du fichier)
                 SommetMot sommet = new SommetMot(mot);
+                // on l'ajoute à notre liste de sommets du graphe
                 this._sommets.add(sommet);
             }
-
             buffer.close();
         }
         catch(Exception ex)
         {
             System.out.println("ERROR " + ex);   
         }
+    }
+    
+    /**
+     * 
+     * @return Retourne le nombre de sommet d'un graphe (Q2)
+     */
+    public int getNbSommets()
+    {
+        return this._sommets.size();
+    }
+    
+    /**
+     * 
+     * @return Retourne le nombre d'arêtes d'un graphe (Q2)
+     */
+    public int getNbAretes()
+    {
+        return 0;
     }
 }   
     
--- src/projetgraphes/ProjetGraphes.java
+++ src/projetgraphes/ProjetGraphes.java
@@ -20,11 +20,20 @@ public class ProjetGraphes
      */
     public static void main(String[] args)
     {
-        
+        loadGraphes();
+    }
+    
+    /**
+     * Cette méthode permet de créer tous les graphes contenant des mots de taille i.
+     * On crée dynamiquement un nom de fichier txt puis on charge l'ensemble de ses mots.
+     */
+    private static void loadGraphes()
+    {
         /** Question 1 : chargement des graphes */
         for(int i = 4; i < 19; i++)
         {
             Graphe g = new Graphe();
+            // nom de fichier dynamique
             String filename = "mots/mots";
             if(i < 10)
             {
@@ -36,8 +45,24 @@ public class ProjetGraphes
                 filename += i;
             }
             filename +="lettres.txt";
+            // on charge les mots du graphes
             g.load(filename);
+            // on l'ajoute à notre liste de graphes
             _graphes.add(g);
          }
+    }
+    
+    
+    /**
+     * QUESTION 2
+     * Affichage de la Q2
+     */
+    private static void getNbSommets_NbAretes_Graphes()
+    {
+        for(int i = 0; i < _graphes.size(); i++)
+        {
+            System.out.println("Le nombre de sommets du " + i + "ème graphe est " + _graphes.get(i).getNbSommets());
+            System.out.println("Le nombre de sommets du " + i + "ème graphe est " + _graphes.get(i).getNbSommets());
+        }
     }
 }
