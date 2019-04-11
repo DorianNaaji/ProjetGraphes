977d113df55096ea16e948a98eff0abfe165c10b
Thiti <t.delplanque92@gmail.com>
2019-04-11 09:07:28 +0200

    ajout méthodes dans SommetMot: ctor,get,set,make&remove relation

--- src/projetgraphes/SommetMot.java
+++ src/projetgraphes/SommetMot.java
@@ -5,6 +5,9 @@
  */
 package projetgraphes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Dorian
@@ -12,5 +15,70 @@ package projetgraphes;
 public class SommetMot
 {
     private String _mot;
+    private List<SommetMot> _relations = new ArrayList<SommetMot>();
+
+    public String getMot() {
+        return _mot;
+    }
+
+    public void setMot(String mot) {
+        this._mot = mot;
+    }
+
+    public List<SommetMot> getRelations() {
+        return _relations;
+    }
+
+    public void setRelations(List<SommetMot> relations) {
+        this._relations = relations;
+    }
+    
+    public SommetMot(String mot){
+        _mot = mot;   
+    }
+    
+    // ajoute un mot dans la liste de façons unilatéral
+    public void addRelation(SommetMot mot){
+        if(mot == null)
+            throw new IllegalArgumentException();
+        if(!_relations.contains(mot)){
+            _relations.add(mot);
+        }
+            
+    }
+    
+    // retire un mot de la liste de façons unilatéral
+    public void removeRelation(SommetMot mot){
+        if(mot == null)
+            throw new IllegalArgumentException();
+        if(_relations.contains(mot)){
+            _relations.remove(mot);
+        }
+    }
+    
+    // créer une relation entre deux sommet
+    public void makeRelation(SommetMot mot){
+        if(mot == null)
+            throw new IllegalArgumentException();
+        if(!_relations.contains(mot)){
+            _relations.add(mot);
+        }
+        if(!mot._relations.contains(mot)){
+            mot._relations.add(mot);
+        }
+    } 
+    
+    // supprime une relation entre deux sommet
+    public void deleteRelation(SommetMot mot){
+        if(mot == null)
+            throw new IllegalArgumentException();
+        if(_relations.contains(mot)){
+            _relations.remove(mot);
+        }
+        if(mot._relations.contains(mot)){
+            mot._relations.remove(mot);
+        }
+    }
+    
     
 }
