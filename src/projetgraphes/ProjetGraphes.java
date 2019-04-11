3833f0b1d0061b1ed23bad6f1f5821b8768b7fc6
Dorian <dorian.naaji@gmail.com>
2019-04-11 09:06:02 +0200

    avaancement

--- src/projetgraphes/ProjetGraphes.java
+++ src/projetgraphes/ProjetGraphes.java
@@ -5,6 +5,8 @@
  */
 package projetgraphes;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Dorian
@@ -12,12 +14,30 @@ package projetgraphes;
 public class ProjetGraphes
 {
 
+    private static ArrayList<Graphe> _graphes = new ArrayList<Graphe>();
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+        
+        /** Question 1 : chargement des graphes */
+        for(int i = 4; i < 19; i++)
+        {
+            Graphe g = new Graphe();
+            String filename = "mots/mots";
+            if(i < 10)
+            {
+               filename += "0";
+               filename += i;
+            }
+            else
+            {
+                filename += i;
+            }
+            filename +="lettres.txt";
+            g.load(filename);
+            _graphes.add(g);
+         }
     }
-    
 }
