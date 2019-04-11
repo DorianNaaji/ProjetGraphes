6c6350198f21ae81ed3c9686b2fca0c5f1c33279
Dorian <dorian.naaji@gmail.com>
2019-04-11 11:16:40 +0200

    Commit

--- src/projetgraphes/Graphe.java
+++ src/projetgraphes/Graphe.java
@@ -10,6 +10,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Map;
 
 /**
  *
@@ -75,7 +76,46 @@ public class Graphe
      */
     public int getNbAretes()
     {
-        return 0;
+        int nbAretes = 0;
+        for(int i = 0; i < this._sommets.size(); i++)
+        {
+            nbAretes += this._sommets.get(i).getRelations().size();
+        }
+        return nbAretes/2;
+    }
+    
+    /**
+     * Question 6 : Sommet de degré Max
+     * @return le sommet de degréMax
+     */
+    public SommetMot SommetDedegreMax()
+    {
+        int nMax = 0;
+        SommetMot sommetDegreMax = null;
+        for(int i = 0; i < this._sommets.size(); i++)
+        {
+            int degreCourant = this._sommets.get(i).getRelations().size();
+           // nMax = (degreCourant > nMax) ? degreCourant : nMax;
+           if(degreCourant > nMax)
+           {
+               nMax = degreCourant;
+               sommetDegreMax = this._sommets.get(i);
+           }
+        }
+        return sommetDegreMax;
+    }
+    
+    public ArrayList<SommetMot> sommetsAvecKvoisins(int k)
+    {
+        ArrayList<SommetMot> sommetsAvecKVoisins = new ArrayList<SommetMot>();
+        for(int i = 0; i < this._sommets.size(); i++)
+        {
+            if(this._sommets.get(i).getRelations().size() == k)
+            {
+                sommetsAvecKVoisins.add(this._sommets.get(i));
+            }
+        }
+        return sommetsAvecKVoisins;
     }
     
     public void display(){
